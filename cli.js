345665ee1881324942c2f1dcf3effd48a955ba9f
Purval Kamble <purval.kamble@capgemini.com>
2022-11-10 16:35:16 +0530

    used Simple-Git Method

--- cli.js
+++ cli.js
@@ -1,13 +1,33 @@
-const {Octokit} = require('octokit');
 
-const octokit = new Octokit({
-    auth:process.env.GITHUB_TOKEN,
-});
+// const { Octokit, App } = require("octokit");
+// const octokit = new Octokit({ auth: `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` });
 
-async function run(){
-    const response = await octokit.request('GET /user');
+// // Compare: https://docs.github.com/en/rest/reference/users#get-the-authenticated-user
 
-    console.log(`Authenticated as ${response.data.login}`);
+
+
+// async function myfunction() {
+//     const {
+//         data: { login },
+//       } = await octokit.rest.users.getAuthenticated();
+//       console.log("Hello, %s", login);
+// }
+
+// myfunction();
+
+
+const simpleGit = require("simple-git");
+const git = simpleGit.default();
+
+async function changeBranch(){
+    await git.checkout("octokitNewbranch");
 };
 
-run(); 
+changeBranch();
+
+async function newFunc(){
+    const branch = await git.branch();
+    console.log(branch.current)
+};
+
+newFunc();
